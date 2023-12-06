855fbecf552e24ed5f5d20a1dcb768d73d761282
ordio <max@maxsamuelsson.se>
2023-12-06 10:50:05 +0100

    Added second method

--- Labb1SQL/Program.cs
+++ Labb1SQL/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Data.SqlClient;
+using System.Transactions;
+
 namespace Labb1SQL
 {
     internal class Program
@@ -14,8 +16,8 @@ namespace Labb1SQL
             }
         }
 
-        // View 1 Method for Case 1
-        static void View1(SqlConnection connection)
+        // Case 1 - View all Students
+        static void ViewAllStudents(SqlConnection connection)
         {
             // Doing a join select so i can display the ClassName from ClassTable together with each student.
             using (SqlCommand command = new SqlCommand("SELECT s.FirstName, s.LastName, c.ClassName FROM Students s JOIN Class c ON s.FK_ClassID = c.ClassID", connection))
@@ -25,7 +27,6 @@ namespace Labb1SQL
                     Console.Clear();
                     Console.WriteLine("List of all students and their class:");
 
-                    // While the reader is reading it will loop thru and print out all the information
                     while (reader.Read())
                     {
                         string firstName = reader.GetString(reader.GetOrdinal("FirstName")).TrimEnd();
@@ -36,9 +37,52 @@ namespace Labb1SQL
                     }
                 }
             }
-            // Using my return to menu method 
             ReturntoMenu(connection);
         }
+
+        // Case 2 - View Students in specific class
+        static void ViewStudentsIn(SqlConnection connection)
+        {
+            Console.Clear();
+            Console.WriteLine("All Classes: ");
+            using (SqlCommand command1 = new SqlCommand("SELECT ClassName FROM Class", connection))
+            {
+                using (SqlDataReader reader = command1.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string className = reader.GetString(reader.GetOrdinal("ClassName")).TrimEnd();
+                        Console.WriteLine($"- {className}");
+                    }
+                }
+            }
+            Console.Write("Enter name of Class to list their students: ");
+            string input = Console.ReadLine();
+
+            string query = "Select s.FirstName, s.LastName, c.ClassName " +
+                "FROM Students s " +
+                "JOIN Class c ON s.FK_ClassID = c.ClassID " +
+                "WHERE c.ClassName = @ClassName";
+
+            using (SqlCommand command1 = new SqlCommand(query, connection))
+            {
+                command1.Parameters.AddWithValue("@ClassName", input);
+
+                using (SqlDataReader reader = command1.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string firstName = reader.GetString(reader.GetOrdinal("FirstName")).TrimEnd();
+                        string lastName = reader.GetString(reader.GetOrdinal("LastName")).TrimEnd();
+                        string className = reader.GetString(reader.GetOrdinal("ClassName")).TrimEnd();
+
+                        Console.WriteLine($"Student: {firstName} {lastName} - {className}");
+                    }
+                }
+            }
+            ReturntoMenu(connection);
+        }
+
         // Method for the StartMenu that includes switch case
         static void StartMenu(SqlConnection connection)
         {
@@ -49,20 +93,23 @@ namespace Labb1SQL
                 "[4] View Staffmembers\n" +
                 "[5] View Grades set for specific Month\n" +
                 "[6] Average grade for each Course\n" +
-                "[7] Add Student\n");
+                "[7] Add Student\n" +
+                "[Q] To Quit Program\n");
             Console.Write("Input: ");
 
             string input = Console.ReadLine();
             switch (input)
             {
                 case "1":
-                    View1(connection);
+                    ViewAllStudents(connection);
                     break;
 
                 case "2":
+                    ViewStudentsIn(connection);
                     break;
 
                 case "3":
+                    AddStaff(connection);
                     break;
 
                 case "4":
@@ -76,17 +123,21 @@ namespace Labb1SQL
 
                 case "7":
                     break;
-
+                case "Q":
+                    Environment.Exit(0);
+                    break;
 
             }
         }
         // Method that is usable to return to menu when u want to exit.
         static void ReturntoMenu(SqlConnection connection)
         {
-            Console.WriteLine("Press Enter to go back to the main menu...");
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("Press [Enter] to go back to the main menu");
+            Console.WriteLine("---------------------------------------------------------");
             // Wait for the Enter key
             while (Console.ReadKey().Key != ConsoleKey.Enter) { }
-            // Clear the console before going back to the StartMenu
+
             Console.Clear();
             StartMenu(connection);
         }
